c5a88c2f3b2b4ce0091eb500466fde9a1887d5a6
Arshdeep Dhindsa <33647156+ArshdeepDhindsa@users.noreply.github.com>
2018-11-12 04:57:49 -0500

    Add files via upload

---
+++ scripts/type.js
@@ -0,0 +1,37 @@
+
+
+setTimeout(function(){
+        $(".e2").css("display", "inherit");
+        $(".e2").typed({
+            strings: ["Software Developer by profession,"],
+            typeSpeed: 50, 
+            startDelay:700,
+            loop: false,
+            loopCount: false, 
+            callback: function(){ } 
+        });
+    }, 2000);
+
+setTimeout(function(){
+        $("e3").css("display", "inherit");
+        $(".e3").typed({
+            strings: ["an enthusiast by heart,"],
+            typeSpeed: 50, 
+            startDelay:1900,
+            loop: false, 
+            loopCount: false, 
+            callback: function(){ } 
+        });
+    }, 4000);
+
+setTimeout(function(){
+        $(".e4").css("display", "inherit");
+        $(".e4").typed({
+            strings: ["always ready to take new Lessons."],
+            typeSpeed: 50, 
+            startDelay:2000,
+            loop: false, 
+            loopCount: false, 
+        });
+    }, 6000);
+
